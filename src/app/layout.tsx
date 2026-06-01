fc24f3a6e326cca58c73f2e2a056730396f26c4f
iadityajrm <aditya.jayaram2468@gmail.com>
2026-06-01 10:10:26 +0530

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,20 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
           rel="stylesheet"
         />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z08J3NSXCN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Z08J3NSXCN');
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">
         {children}
